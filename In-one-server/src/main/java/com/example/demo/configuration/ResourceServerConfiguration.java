7361929c893b87297ca469b6d3e3259c6dc495cc
sok menghok <soksomeungkhun@gmail.com>
2019-11-26 22:57:09 +0700

    add authorization code grant type

--- In-one-server/src/main/java/com/example/demo/configuration/ResourceServerConfiguration.java
+++ In-one-server/src/main/java/com/example/demo/configuration/ResourceServerConfiguration.java
@@ -2,10 +2,12 @@ package com.example.demo.configuration;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@Order(2)
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
@@ -13,6 +15,6 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .regexMatchers("/secure").access("#oauth2.hasScope('write')")
-                .anyRequest().permitAll();
+                .anyRequest().permitAll().and().formLogin();
     }
 }
--- In-one-server/src/main/java/com/example/demo/configuration/WebSecurityConfiguration.java
+++ In-one-server/src/main/java/com/example/demo/configuration/WebSecurityConfiguration.java
@@ -2,11 +2,12 @@ package com.example.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+@Order(1)
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -23,4 +24,5 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
 }
